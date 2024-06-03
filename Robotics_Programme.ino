ebbab8da728fbd0f82f6c14b6b455aafd4433870
maksymilian-Mroczkowski <mm2023@ic.ac.uk>
2024-06-03 14:50:20 +0100

    Add files via upload

---
+++ Robotics_Programme.ino
@@ -0,0 +1,57 @@
+// this programme allows for the controlling of servos by potentialometers, along with the ability to turn on and of an inducible magnet with a mechanical switch.
+
+
+#include <Servo.h>
+
+Servo myservo;   // create servo object to control servo1
+Servo myservo2;  // create servo object to control servo2
+Servo myservo3;  // create servo object to control servo3
+
+int potpin = A0;   // analog pin used to connect the potentiometer
+int val;           // variable to read the value from the analog pin
+
+int potpin2 = A1;  // analog pin used to connect the second potentiometer
+int val2;          // variable to read the value from the second analog pin
+
+int potpin3 = A2;  // analog pin used to connect the third potentiometer
+int val3;          // variable to read the value from the third analog pin
+
+int button_input = 6;  // digital pin used to connect the button
+int EM_output = 7;     // digital pin used to control the electromagnet
+int state = 0;         // variable to store the button state
+bool EM_POWER = false; // boolean to store the state of the electromagnet
+
+void setup() {
+
+  myservo.attach(8); // Attach servos to corresponding pins 
+  myservo2.attach(4);
+  myservo3.attach(3);
+
+  pinMode(8, OUTPUT); // Set pin modes for servo pins, button input, and electromagnet output
+  pinMode(4, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(button_input, INPUT);
+  pinMode(EM_output, OUTPUT);
+}
+
+void loop() {
+
+  if (digitalRead(button_input) == HIGH) { // Check if the button is pressed
+    EM_POWER = !EM_POWER; // Toggle the electromagnet power state
+    digitalWrite(EM_output, EM_POWER); // Set the electromagnet output based on the new power state
+  }
+
+  
+  val = analogRead(potpin); // Read the value from the first potentiometer
+  val = map(val, 0, 1023, 0, 180); // Map the value to a range of 0 to 180 degrees
+  myservo.write(val); // Write the mapped value to the first servo
+
+
+  val2 = analogRead(potpin2);
+  val2 = map(val2, 0, 1023, 0, 180);
+  myservo2.write(val2);
+
+  val3 = analogRead(potpin3);
+  val3 = map(val3, 0, 1023, 0, 180);
+  myservo3.write(val3);
+}
